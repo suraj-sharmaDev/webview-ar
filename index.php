01ec3e6bb2e49b766596df0ccf50de5ab65ca52b
Suraj Sharma <surajdroid97@gmail.com>
2019-04-20 10:50:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,8 @@ recognition.addEventListener('result', e => {
 	const transcript = Array.from(e.results)
 		.map(result => result[0])
 		.map(result => result.transcript)
-		.join('')
+		.join('');
+	p.textContent = transcript;
 	console.log(transcript);
 });
 

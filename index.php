5632145b8ae52ae2075956ad643b5ca3accbca36
suraj-sharmaDev <surajdroid97@gmail.com>
2019-04-20 11:27:06 +0530

    New php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>WebView Ar</title>
+</head>
+<body>
+	<div class="box">
+		<p id="subtitle"></p>
+	</div>
+</body>
+<script>
+var recognition = new webkitSpeechRecognition();
+recognition.continuous = true;
+recognition.interimResults = true;
+let p=document.getElementById("subtitle");
+let msg="";
+recognition.addEventListener('result', e => {
+	const transcript = Array.from(e.results)
+		.map(result => result[0])
+		.map(result => result.transcript)
+		.join('');
+	p.innerHTML = transcript;	
+});
+recognition.addEventListener('end', recognition.start);
+recognition.start();
+</script>
+</html>

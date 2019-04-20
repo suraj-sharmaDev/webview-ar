c15e2412b60f1bcba367b53bcc98b103a154def1
suraj-sharmaDev <surajdroid97@gmail.com>
2019-04-20 11:49:33 +0530

    done

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ recognition.addEventListener('result', e => {
 	let transcript = Array.from(e.results)
 		.map(result => result[0])
 		.map(result => result.transcript)
-	
+		.join('');
 	console.log(transcript);
 });
 recognition.addEventListener('end', recognition.start);

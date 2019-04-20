e767fa6db40b8726a560e9c1e10474d9df693ab6
suraj-sharmaDev <surajdroid97@gmail.com>
2019-04-20 11:41:26 +0530

    done

--- index.php
+++ index.php
@@ -17,11 +17,14 @@ recognition.interimResults = true;
 let p=document.getElementById("subtitle");
 let msg="";
 recognition.addEventListener('result', e => {
-	const transcript = Array.from(e.results)
+	let transcript = Array.from(e.results)
 		.map(result => result[0])
 		.map(result => result.transcript)
 		.join('');
-	console.log(transcript.length);
+	if(transcript.length > 100)
+	{
+		transcript="";
+	}
 	p.innerHTML = transcript;	
 });
 recognition.addEventListener('end', recognition.start);

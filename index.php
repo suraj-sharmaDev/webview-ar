7bc03560f0d4fc2f6ae24f1a212eb5af326e69a2
Suraj Sharma <surajdroid97@gmail.com>
2019-04-20 10:33:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Web Voice</title>
-</head>
-<body>
-	<div id="box">
-		<p id="message"></p>
-	</div>
-</body>
-<script type="text/javascript">
-	
-	var msg = document.getElementById('message');
-	window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-
-	var recognition = new SpeechRecognition();
-	recognition.interimResults = true;
-	recognition.lang = 'en-Us';
-
-	recognition.addEventListeners('result', e => [console.log(e)]);
-
+<script>
+var recognition = new webkitSpeechRecognition();
+recognition.continuous = true;
+recognition.interimResults = true;
+recognition.onresult = function(event) { 
+  console.log(event) 
+}
+recognition.start();
 </script>
-</html>

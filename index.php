46796776b932baa8488787759caef22c782a2cd4
Suraj Sharma <surajdroid97@gmail.com>
2019-04-20 10:43:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>WebView Ar</title>
+</head>
+<body>
+	<div class="words">
+	</div>
+</body>
 <script>
 var recognition = new webkitSpeechRecognition();
 recognition.continuous = true;
 recognition.interimResults = true;
-recognition.onresult = function(event) { 
-  console.log(event) 
-}
+
+let p=document.createElement("p");
+const words = document.querySelector('.words');
+words.appendChild(p);
+
+recognition.addEventListener('result', e => {
+	console.log(e);
+});
 recognition.start();
 </script>
+</html>

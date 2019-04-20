8973b0f73c9e7ed7fb65ab773819adfa015ef6e3
Suraj Sharma <surajdroid97@gmail.com>
2019-04-20 10:46:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -18,8 +18,13 @@ const words = document.querySelector('.words');
 words.appendChild(p);
 
 recognition.addEventListener('result', e => {
-	console.log(e);
+	console.log(e.results);
+	const transcript = Array.from(e.results)
+		.map(result => result[0])
+		.map(result => result.transcript)
+	console.log(transcript);
 });
+
 recognition.start();
 </script>
 </html>

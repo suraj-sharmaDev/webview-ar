7621cb746fe549ea624da2f489b9df9a264c8448
suraj-sharmaDev <surajdroid97@gmail.com>
2019-04-20 11:38:33 +0530

    done

--- index.php
+++ index.php
@@ -19,8 +19,6 @@ let msg="";
 recognition.addEventListener('result', e => {
 	const transcript = Array.from(e.results)
 		.map(result => result[0])
-
-<<<<<<< HEAD
 		.map(result => result.transcript)
 		.join('');
 	p.innerHTML = transcript;	
@@ -29,15 +27,3 @@ recognition.addEventListener('end', recognition.start);
 recognition.start();
 </script>
 </html>
-=======
-		.map(result => result.transcript);
-	msg.concat(" ",transcript);
-	p.innerHTML = msg;	
-});
-
-recognition.addEventListener('end', recognition.start);
-
-recognition.start();
-</script>
-</html>
->>>>>>> c96052019e275e87e5f023a667bd2e99f461494b

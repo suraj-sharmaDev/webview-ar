1855c3598829fc84822df6cfb2d001d01d1d9acf
Suraj Sharma <surajdroid97@gmail.com>
2019-04-21 11:26:30 +0530

    done

--- index.php
+++ index.php
@@ -3,13 +3,43 @@
 <head>
 	<meta charset="UTF-8">
 	<title>WebView Ar</title>
+    <style>
+    #container {
+        margin: 0px auto;
+        width: 500px;
+        height: 375px;
+        border: 10px #333 solid;
+    }
+    #videoElement {
+        width: 500px;
+        height: 375px;
+        background-color: #666;
+    }
+    </style>    
 </head>
 <body>
-	<div class="box">
-		<p id="subtitle"></p>
-	</div>
+<div id="container">
+	<video autoplay="true" id="videoElement">
+	
+	</video>
+</div>    
+<div class="box">
+    <p id="subtitle"></p>
+</div>
 </body>
 <script>
+var video = document.querySelector("#videoElement");
+
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+    })
+    .catch(function (error) {
+      console.log("Something went wrong!");
+    });
+}
+    
 var recognition = new webkitSpeechRecognition();
 recognition.continuous = true;
 recognition.interimResults = false;
